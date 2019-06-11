d2d0643c285f609971d1c55a7c0893e8232c0900
Adam <adam.e.sheaffer@gmail.com>
2019-06-11 10:55:09 -0500

    run functions on node 8. log message to firebase console

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.onAwardCreated = functions.firestore
+  .document('cohorts/{cohortId}/awards/{awardId}')
+  .onCreate(async (awardSnapshot, context) => {
+    const award = awardSnapshot.data();
+
+    const cohortId = context.params.cohortId;
+    const cohortSnapshot = await admin.firestore()
+      .doc(`cohorts/${cohortId}`)
+      .get();
+    const cohort = cohortSnapshot.data();
+
+    const message = `${award.createdBy} has added a new ProGrammys award for ${cohort.name}: ${award.name}`;
+    console.log(message);
+
+  });
