7078fa115b9b63ce78c8be831a65e8e5dc2f6e74
Adam <adam.e.sheaffer@gmail.com>
2019-06-13 13:59:13 -0500

    remove webhook and use slack api. update travis to install deps

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,36 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const axios = require('axios');
+const { buildMessage } = require('./slackMessageBuilder');
+
+const slackURL = 'https://slack.com/api/chat.postMessage';
 
 admin.initializeApp();
+
+// set auth headers for slack requests
+axios.interceptors.request.use((req) => {
+  if (req.url.startsWith('https://slack.com')) {
+    const token = process.env.SLACK_TOKEN;
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
+axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 
 exports.onAwardCreated = functions.firestore
   .document('cohorts/{cohortId}/awards/{awardId}')
   .onCreate(async (awardSnapshot, context) => {
     const award = awardSnapshot.data();
 
-    const cohortId = context.params.cohortId;
+    const { cohortId } = context.params;
     const cohortSnapshot = await admin.firestore()
       .doc(`cohorts/${cohortId}`)
       .get();
     const cohort = cohortSnapshot.data();
 
-    const text = `${award.createdBy} has added a new ProGrammys award for ${cohort.name}\n*${award.name.toUpperCase()}*`;
-    const attachments = [
-      {
-        fallback: 'Vote at https://the-programmys.web.app',
-        actions: [
-          {
-            type: 'button',
-            text: 'Vote ✔',
-            url: 'https://the-programmys.web.app',
-            style: 'primary'
-          }
-        ]
-      }
-    ];
+    const slackPayload = buildMessage(award, cohort);
 
-    return axios.post(process.env.slackUrl, { text, attachments });
+    return axios.post(slackURL, slackPayload)
+      .then(({ data }) => data);
   });
