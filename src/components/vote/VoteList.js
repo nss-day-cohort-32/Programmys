80331d26d7f5d172d4cb4b0f60c288715645c81a
BryanNilsen <bryannilsen@gmail.com>
2019-06-26 18:39:43 -0500

    added vote functionality

--- src/components/vote/VoteList.js
+++ src/components/vote/VoteList.js
@@ -8,11 +8,11 @@ class VoteList extends Component {
     super(props);
     this.state = { awards: [] };
     this.db = firebase.firestore();
+    this.submitVote = this.submitVote.bind(this);
   }
 
   componentDidMount() {
-    const db = firebase.firestore();
-    db.collection('cohorts/D32/awards').get()
+    this.db.collection('cohorts/D32/awards').get()
       .then((querySnapshot) => {
         const awards = querySnapshot.docs.map(doc => ({
           ...doc.data(),
@@ -31,13 +31,18 @@ class VoteList extends Component {
     });
   }
 
+  submitVote(awardId, voteObject) {
+    console.log('Voted', awardId, voteObject);
+    this.db.collection(`cohorts/D32/awards/${awardId}/votes`).doc().set(voteObject);
+  }
+
   render() {
-    //
     const unvotedAwards = this.getUnvotedAwards();
+    const { currentUser } = this.props;
     return (
       <>
         <h1>Awards</h1>
-        {unvotedAwards.map(award => <VoteItem key={award.id} award={award} />)}
+        {unvotedAwards.map(award => <VoteItem key={award.id} award={award} submitVote={this.submitVote} currentUser={currentUser} />)}
       </>
     );
   }
